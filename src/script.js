e1b1f1c24b825b0b5e93e9cd70ff8842f152a38d
Matthew Pangan <akio.pangan@gmail.com>
2022-12-04 00:02:43 +0800

    added accordions

--- src/script.js
+++ src/script.js
@@ -1,8 +1,35 @@
-function ExpandingCards() {
+window.onload = main; //Calls main function when window loads
+
+/*
+*
+*		THE MAIN FUNCTION
+*	where the code will be executed
+* */
+
+function SetupAccordionButton()
+{
+	document.querySelectorAll(".accordion-button").forEach(button => 
+	{
+		button.addEventListener("click", () => 
+			{
+				const accordionContent = button.nextElementSibling;
+
+				button.classList.toggle("accordion-button-active");
+				if (button.classList.contains("accordion-button-active"))
+				{
+					accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+				}
+				else
+				{
+					accordionContent.style.maxHeight = 0;
+				}
+
+			});
+	});
 }
 
-function ScrollAnimation() {
-}
-
-function FAQCollapse() {
+function main() 
+{
+	SetupAccordionButton();
+	
 }
